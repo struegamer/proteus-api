423a28b2f5c58fe2d0c6283f729addc21a05b241
Stephan Adig <sh@sourcecode.de>
2012-10-22 14:24:56 +0200

    PEP8 Changes

--- setup.py
+++ setup.py
@@ -2,18 +2,15 @@
 
 from setuptools import setup
 
-setup(
-    name='python-proteus', 
-    version='0.1', 
-    author='Stephan Adig', 
-    author_email='sh@sourcecode.de', 
-    description='A Proteus IPAM Python Library', 
-    license='LGPLv2', 
-    url='https://github.com/sadig/proteus-api', 
-    packages=[
-        'proteus', 
-        'proteus.api', 
-        'proteus.objects'
-    ], 
-    install_requires=['suds'], 
-)
+setup(name='python-proteus',
+      version='0.1',
+      author='Stephan Adig',
+      author_email='sh@sourcecode.de',
+      description='A Proteus IPAM Python Library',
+      license='LGPLv2', url='https://github.com/sadig/proteus-api',
+      packages=[
+        'proteus',
+        'proteus.api',
+        'proteus.objects'],
+      install_requires=['suds']
+      )
